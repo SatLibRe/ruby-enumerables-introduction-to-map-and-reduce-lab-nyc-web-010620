2af1e870df371cb0f617b04f7222a18462cb27fb
Miles Higbie <miles.higbie@gmail.com>
2019-12-17 20:58:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -24,7 +24,7 @@ def map_to_square(source_array)
   end
 end 
 
-def reduce_to_total(source_array, starting_point = nil)
+def reduce_to_total(source_array, starting_point)
   source_array(starting_point).reduce do |sum,n|
      sum + n 
   end 
